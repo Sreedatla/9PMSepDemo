67c6d6c5b76c8a394e9d34540f0a2fbe1ad226ad
sreedatla <sreedatla92@gmail.com>
2020-09-08 12:14:04 -0400

    Signed-off-by: sreedatla <sreedatla92@gmail.com>

---
+++ src/test/java/testdemo/HomeLoanTestScript1.java
@@ -0,0 +1,20 @@
+package testdemo;
+
+import org.testng.annotations.Test;
+
+public class HomeLoanTestScript1 {
+	
+	
+
+	
+		@Test
+		public void Homeloan()
+		
+		{
+			 System.out.println("Homeloan functionality");
+			
+		}
+	}
+
+
+}
