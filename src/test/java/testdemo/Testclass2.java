8f60100930ebec6055c5d814bacf10823352f8c5
sreedatla <sreedatla92@gmail.com>
2020-09-08 12:04:25 -0400

    Signed-off-by: sreedatla <sreedatla92@gmail.com>

---
+++ src/test/java/testdemo/Testclass2.java
@@ -0,0 +1,13 @@
+package testdemo;
+
+import org.testng.annotations.Test;
+
+public class Testclass2 {
+	@Test
+	public void logout()
+	
+	{
+		 System.out.println("loginto functionality");
+		
+	}
+}
