356fbda418f0d479b5262c147b4c2778363061ce
sreedatla <sreedatla92@gmail.com>
2020-09-09 17:09:36 -0400

    Signed-off-by: sreedatla <sreedatla92@gmail.com> created new demp

---
+++ src/test/java/testdemo/SreeDemo1.java
@@ -0,0 +1,17 @@
+package testdemo;
+
+public class SreeDemo1 {
+	
+	
+	public void method1()
+	
+	{
+
+		System.out.println("hello world");
+		System.out.println("hello Sree");
+		System.out.println("hello class");
+	}
+	
+	
+
+}
